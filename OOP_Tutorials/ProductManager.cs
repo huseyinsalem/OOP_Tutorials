109f5b0d66e2961a37b504c2f5c46ac2dc9179ca
huseyinsalem <huseyinsalem65@gmail.com>
2025-07-19 16:36:26 +0300

    Add project files.

---
+++ OOP_Tutorials/ProductManager.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOP_Tutorials
+{
+    internal class ProductManager
+    {
+        public void Add(Product product)
+        {
+            Console.WriteLine(product.ProductName + "Eklendi");
+
+        }
+     
+    }
+}
---
+++ OOP_Tutorials/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOP_Tutorials
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Product product1 = new Product
+            {
+                Id = 1,
+                CategoryId = 2,
+                ProductName = "Table",
+                UnitPrice = 500,
+                UnitsInStock = 4,
+            };
+            Product product2 = new Product
+            {
+                Id = 2,
+                CategoryId = 3,
+                ProductName = "Chair",
+                UnitPrice = 800,
+                UnitsInStock = 6,
+            };
+
+
+
+            ProductManager productManager = new ProductManager();
+            productManager.Add(product1);
+            Console.WriteLine(product1.ProductName);
+
+
+
+
+            Console.Read();
+        }
+    }
+}
