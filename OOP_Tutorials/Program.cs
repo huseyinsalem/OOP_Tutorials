f4985ffd828e7113e08908d61c256e68e748e8b5
huseyinsalem <huseyinsalem65@gmail.com>
2025-07-20 11:30:53 +0300

    Implemented data operations using List

--- OOP_Tutorials/Program.cs
+++ OOP_Tutorials/Program.cs
@@ -10,6 +10,8 @@ namespace OOP_Tutorials
     {
         static void Main(string[] args)
         {
+
+            #region class ve metotlar
             Product product1 = new Product
             {
                 Id = 1,
@@ -32,6 +34,28 @@ namespace OOP_Tutorials
             ProductManager productManager = new ProductManager();
             productManager.Add(product1);
             Console.WriteLine(product1.ProductName);
+            #endregion
+
+
+
+            #region List
+
+            Console.WriteLine("-----------");
+
+
+
+            List<string> Name =new List<string> {"Hüseyin","Hasan","Handan","Ali" };
+            Console.WriteLine(Name[0]);
+            Console.WriteLine(Name[1]);
+            Console.WriteLine(Name[2]);
+            Console.WriteLine(Name[3]);
+
+            Name.Add("Neşe");
+            Console.WriteLine(Name[4]);
+            Console.WriteLine(Name[0]);
+
+            #endregion
+
 
 
 
